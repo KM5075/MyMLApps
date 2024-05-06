a2adc442c7a91be50ddf67227023a81321e2d6a9
KM5075 <koji19991999@gmail.com>
2024-05-06 20:27:32 +0900

    ML.NETの最初のチュートリアル実施

--- MyMLApps/Program.cs
+++ MyMLApps/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using MyMLApps;
+// Add input data
+var sampleData = new SentimentModel.ModelInput()
+{
+    //Col0 = "This restaurant was wonderful."
+    //Col0 = "Loved this place."
+    Col0 = "It was bad."
+};
+
+// Load model and predict output of sample data
+var result = SentimentModel.Predict(sampleData);
+
+// If Prediction is 1, sentiment is "Positive"; otherwise, sentiment is "Negative"
+var sentiment = result.PredictedLabel == 1 ? "Positive" : "Negative";
+Console.WriteLine($"Text: {sampleData.Col0}\nSentiment: {sentiment}");
