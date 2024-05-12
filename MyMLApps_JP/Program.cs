6611e1a57093a963d7faa1f53d06c6d9d1039dd7
KM5075 <koji19991999@gmail.com>
2024-05-12 21:21:17 +0900

    学習対象のテキストを日本語に変更し学習を実施したConsoleアプリを作成

---
+++ MyMLApps_JP/Program.cs
@@ -0,0 +1,13 @@
+﻿using MyMLApps_JP;
+// Add input data
+var sampleData = new SentimentModel_JP.ModelInput()
+{
+    Col0 = "なぜこの悲しい小さな野菜はそんなに加熱しすぎているのでしょうか？"
+};
+
+// Load model and predict output of sample data
+var result = SentimentModel_JP.Predict(sampleData);
+
+// If Prediction is 1, sentiment is "Positive"; otherwise, sentiment is "Negative"
+var sentiment = result.PredictedLabel == 1 ? "Positive" : "Negative";
+Console.WriteLine($"Text: {sampleData.Col0}\nSentiment: {sentiment}");
