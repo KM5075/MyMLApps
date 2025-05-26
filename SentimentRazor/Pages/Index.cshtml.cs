98d1bce61aa5e43f645f3c603f8755a73a4d7cca
KM5075 <koji19991999@gmail.com>
2025-05-26 20:45:42 +0900

    IndexModelにPredictionEnginePoolフィールドを追加
    `Microsoft.Extensions.ML` と `SentimentRazor.SentimentRazor` をインポート。
    `PredictionEnginePool<ModelInput, ModelOutput>` 型の
    `_predictionEnginePool` フィールドを `IndexModel` クラスに追加。
    コンストラクタでの初期化は未実施。

--- SentimentRazor/Pages/Index.cshtml.cs
+++ SentimentRazor/Pages/Index.cshtml.cs
@@ -1,11 +1,14 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.ML;
+using static SentimentRazor.SentimentRazor;
 
 namespace SentimentRazor.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly PredictionEnginePool<ModelInput, ModelOutput> _predictionEnginePool;
 
         public IndexModel(ILogger<IndexModel> logger)
         {
