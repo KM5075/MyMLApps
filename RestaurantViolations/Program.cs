e390043be328d0d1202222c8aab90a61110af772
KM5075 <koji19991999@gmail.com>
2025-06-09 19:54:17 +0900

    モデルの学習

--- RestaurantViolations/Program.cs
+++ RestaurantViolations/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        var sampleData = new RestaurantViolations.RestaurantViolations.ModelInput()
+        {
+            InspectionType = "Routine",
+            ViolationDescription = "Improper food storage",
+        };
+
+        var predictedLabels = RestaurantViolations.RestaurantViolations.PredictAllLabels(sampleData);
+        Console.WriteLine("Predicted Labels:");
+        foreach (var label in predictedLabels)
+        {
+            Console.WriteLine($"{label.Key}: {label.Value}");
+        }
+        Console.WriteLine("Press any key to exit...");
+        Console.ReadKey();
+    }
+}
