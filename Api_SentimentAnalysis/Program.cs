17defa7be698bdaabe24633d820d245645ccea57
KM5075 <koji19991999@gmail.com>
2025-08-03 15:45:46 +0900

    データ型の追加

--- Api_SentimentAnalysis/Program.cs
+++ Api_SentimentAnalysis/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Microsoft.ML;
+using Microsoft.ML.Data;
+using static Microsoft.ML.DataOperationsCatalog;
+
+using Api_SentimentAnalysis;
+
+string _dataPath = Path.Combine(Environment.CurrentDirectory, "Data", "yelp_labelled.txt");
+
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
